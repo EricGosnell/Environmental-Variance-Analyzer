c183743a1bbcd93c1c7d46bec3e298360ec237f3
WillL <willlockhart65@gmail.com>
2025-11-05 18:23:57 -0700

    fixed redirect

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,9 @@ app.use(express.static(path.join(__dirname, 'Pages')));
 
 // Default route -> home page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Pages', 'home', 'home.html'));
+  res.redirect('/home/home.html');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
