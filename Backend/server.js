d3387d7fcf1583dbad40d0fae1660403b3240304
WillL <willlockhart65@gmail.com>
2025-11-05 18:17:35 -0700

    small change

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve all static files under /Pages
 app.use(express.static(path.join(__dirname, 'Pages')));
 
 // Default route -> home page
