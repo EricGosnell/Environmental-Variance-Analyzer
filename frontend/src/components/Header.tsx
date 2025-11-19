fe836ab41682fd85240cc8c417985291170b4a1f
Eric Gosnell <eric.gosnell@colorado.edu>
2025-11-19 11:12:49 -0700

    Navbar styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
+import cosgc_logo from "../assets/COSGC_Logo.jpeg"
 
 export default function Header() {
     return (
         <header className="navbar">
-            <div className="navbar-logo"><Link to="/">Environmental Variance Analyzer</Link></div>
+            <div className="nav-container">
+                <div className="navbar-home">
+                    <Link to="/">Environmental Variance Analyzer</Link>
+                </div>
 
-            <nav className="navbar-links">
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/profile">Profile</Link>
-            </nav>
+                <nav className="navbar-links">
+                    <NavLink to="/">Map</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/profile">Profile</NavLink>
+                    <div className="nav-separator"></div>
+                    <Link to="https://www.colorado.edu/center/spacegrant/" target="_blank">
+                        <img
+                            src={cosgc_logo}
+                            alt="COSGC Logo"
+                            className="cosgc-logo"
+                        />
+                    </Link>
+                    <Link to="https://www.nasa.gov/" target="_blank">
+                        <img
+                            src="https://www.nasa.gov/wp-content/themes/nasa/assets/images/nasa-logo.svg"
+                            alt="NASA Logo"
+                            className="nasa-logo"
+                        />
+                    </Link>
+
+                </nav>
+            </div>
         </header>
     );
 }
