5767662a2af6b51aded4f90131b8437c5273112a
Eric Gosnell <eric.gosnell@colorado.edu>
2025-11-20 12:08:04 -0700

    Simplify homepage CSS

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,9 +3,9 @@ export default function Home() {
         <div className="homepage-container">
             <div className="controls-container">
                 <p>Controls</p>
-                <button className="primary-btn">Add EVA Pod</button>
+                <button className="btn primary-btn">Add EVA Pod</button>
                 <br/>
-                <button className="secondary-btn">Edit EVA Pod</button>
+                <button className="btn secondary-btn">Edit EVA Pod</button>
 
                 <div className="filters-container">
                     <p>Filters</p>
