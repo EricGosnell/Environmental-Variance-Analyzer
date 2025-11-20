010d9036609ce1e0d420b49ef440b84663129337
Eric Gosnell <eric.gosnell@colorado.edu>
2025-11-19 17:01:39 -0700

    Started home page styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <div className="homepage-container">
+            <div className="controls-container">
+                <p>Controls</p>
+                <button className="primary-btn">Add EVA Pod</button>
+                <br/>
+                <button className="secondary-btn">Edit EVA Pod</button>
+
+                <div className="filters-container">
+                    <p>Filters</p>
+                </div>
+            </div>
+            <div className="map-container">
+                <p>Map Here</p>
+            </div>
+        </div>
+    )
 }
