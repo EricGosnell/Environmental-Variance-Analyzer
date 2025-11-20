af53f67804b4520a7709b4c817549f156ef7cd71
Eric Gosnell <eric.gosnell@colorado.edu>
2025-11-20 12:51:53 -0700

    Add satellite imagery

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,16 +1,28 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, LayersControl, ZoomControl } from "react-leaflet";
 
 export default function MapView() {
     return (
         <MapContainer
             center={[40, -105.26]}
             zoom={12}
+            zoomControl={false}
             style={{ width: "100%", height: "100%" }}
         >
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution="&copy; OpenStreetMap contributors"
-            />
+
+        <ZoomControl position="bottomright" />
+        <LayersControl position="bottomright">
+            <LayersControl.BaseLayer name="OpenStreetMap" checked>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                           attribution="&copy; OpenStreetMap contributors"
+                />
+            </LayersControl.BaseLayer>
+
+            <LayersControl.BaseLayer name="ESRI Satellite">
+                <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                           attribution="Tiles © Esri — Source: Esri, Maxar, Earthstar Geographics"/>
+            </LayersControl.BaseLayer>
+        </LayersControl>
+
         </MapContainer>
     );
 }
