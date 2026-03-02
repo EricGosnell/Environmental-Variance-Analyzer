67aab547b39aa10fec9c8ab0d644dfedea3b45f7
kayla <kaylazhong1@gmail.com>
2026-03-02 12:47:16 -0700

    added more content to info pages

--- frontend/src/pages/About-meetCARMA.tsx
+++ frontend/src/pages/About-meetCARMA.tsx
@@ -1,4 +1,47 @@
 import "../styles/InfoPages.css";
+import headshot_barbra_sobhani from "../assets/Headshot_Barbra_Sobhani.jpg";
+import headshot_annie_strange from "../assets/Headshot_Annie_Strange.jpg";
+import headshot_collette_wilfong from "../assets/Headshot_Collette_Wilfong.jpg";
+import headshot_mark_kettles from "../assets/Headshot_Mark_Kettles.jpg";
+import cosgc_logo from "../assets/COSGC_Logo.jpeg";
+
+const members = [
+    {
+        id: 1,
+        name: "Barbra Sobhani",
+        role: "Director",
+        email: "barbra.sobhani@colorado.edu",
+        image: headshot_barbra_sobhani
+    },
+    {
+        id: 2,
+        name: "Annie Strange",
+        role: "Program Manager",
+        email: "annie.strange@colorado.edu",
+        image: headshot_annie_strange
+    },
+    {
+        id: 3,
+        name: "Collette Wilfong",
+        role: "Manager of Student Projects",
+        email: "collette.wilfong@colorado.edu",
+        image: headshot_collette_wilfong
+    },
+    {
+        id: 4,
+        name: "Mark Kettles",
+        role: "Research Manager",
+        email: "mark.kettles@colorado.edu",
+        image: headshot_mark_kettles
+    },
+    {
+        id: 5,
+        name: "Maura Thomas",
+        role: "CARMA Project Manager",
+        email: "math8010@colorado.edu",
+        image: cosgc_logo
+    }
+]
 
 export default function About_MeetCarma() {
     return (
@@ -14,7 +57,7 @@ export default function About_MeetCarma() {
                     </p>
                     <h2 className="info-subheader">CARMA and STELLA</h2>
                     <p className="info-content">
-                        One of CARMA’s projects includes using NASA’s STELLA spectroscopy device to test its capabilities in measuring the impacts of climate change on plant health across a range of vulnerable environments. By reading the reflected levels of Near-Infared (NIR) and visible red, green, and blue wavelengths, CARMA can detect physiological stress in plants before any visual symptoms appear. CARMA also works to validate the accuracy and reliability of the STELLA device for broader use in climate research, environmental monitoring, and agricultural applications.
+                        One of CARMA’s projects includes using NASA’s STELLA spectroscopy device to test its capabilities in measuring the impacts of climate change on plant health across a range of vulnerable environments. By reading the reflected levels of Near-Infrared (NIR) and visible red, green, and blue wavelengths, CARMA can detect physiological stress in plants before any visual symptoms appear. CARMA also works to validate the accuracy and reliability of the STELLA device for broader use in climate research, environmental monitoring, and agricultural applications.
                     </p>
                     <p className="info-content">
                         For more information on CARMA, including related programs, see NASA Colorado Space Grant Consortium’s{" "}
@@ -22,6 +65,31 @@ export default function About_MeetCarma() {
                             Earth System Projects.
                         </a>
                     </p>
+                    <h2 className="info-subheader">COSGC</h2>
+                    <p className="info-content">
+                        CARMA is proud to be part of the Colorado Space Grant Consortium (COSGC), which is a state-wide program that provides Colorado students access to space through innovative courses and NASA-aligned projects. More information on the COSGC can be found on{" "}
+                        <a href="https://www.colorado.edu/center/spacegrant/" target="_blank" className="info-link">
+                            their website.
+                        </a>
+                    </p>
+                    <h2 className="info-subheader">CARMA's Leadership</h2>
+                    <p className="info-content">
+                        Along with a dedicated team of students, experts, and researchers, the leadership of COSGC and CARMA oversees the diverse range of projects CARMA executes.
+                    </p>
+                    <div className="members-container">
+                        {members.map(member => (
+                            <div key={member.id} className="members-card">
+                                <img src={member.image} alt={member.name} className="members-image" />
+                                <div className="members-content">
+                                    <h3>{member.name}</h3>
+                                    <p>{member.role}</p>
+                                    <a href={member.email} target="_blank" className="info-link">
+                                        {member.email}
+                                    </a>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/pages/About-theEVAPod.tsx
+++ frontend/src/pages/About-theEVAPod.tsx
@@ -8,17 +8,20 @@ export default function About_TheEVAPod() {
                     <h1 className="info-title">About the EVA Pod</h1>
                 </div>
                 <div className="info-body">
-                    <h2 className="info-subheader">Overview</h2>
+                    <h2 className="info-subheader">What It Is</h2>
+                    <p className="info-content">
+                        The EVA (Environmental Variance Analyzer) Pod is a self-contained, field-deployable device designed to autonomously monitor and record key environmental measurements over extended periods of time. Comprised of the EVA unit, which measures temperature, humidity, carbon dioxide, and light, along with the MOSS (Mushroom Off-gassing Soil Sensor) that tracks soil properties and off-gassing, the EVA Pod enables reliable, consistent data collection in remote locations and supports seamless data integration with other pods online.
+                    </p>
+                    <p>
+                        The data collected by the EVA Pod provides comprehensive background information on local environmental conditions which can be used for detailed comparisons with readings taken using the NASA STELLA Module. Together, users can identify real-time, on-site measurements with longer-term environmental patterns. This combined approach supports thorough analysis of ecosystem conditions, climates, and changes, making the EVA Pod an effective tool for environmental research.
+                    </p>
+                    <h2 className="info-subheader">How It Works</h2>
                     <p className="info-content">
 
                     </p>
                     <h2 className="info-subheader">Goals and Objectives</h2>
                     <p className="info-content">
-
-                    </p>
-                    <h2 className="info-subheader">How It Works</h2>
-                    <p className="info-content">
-
+                        The EVA Pod aims to expand access to long-term environmental monitoring by serving as a reliable tool for gathering analytical data. Its intentions include the promotion of hands-on engagement in environmental research and to foster collaboration through its interactive online platform. By enabling approachable methods for collecting and sharing data, the EVA Pod supports the development of deeper understandings of local environmental conditions at any location possible.
                     </p>
                 </div>
             </div>
