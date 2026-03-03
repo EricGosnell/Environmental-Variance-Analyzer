6ebec422b752bd9b3e7520b648eb1ec8943a73da
kaylaz0 <kaylazhong1@gmail.com>
2026-03-03 15:42:41 -0700

    bug fixes, implemented logout

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,33 +9,33 @@ export default function Footer() {
                 <div className="footer-column">
                     <h4>Explore</h4>
                     <Link to="/">Map</Link>
-                    <Link to="/about-assembly-instructions">Assembly Instructions</Link>
+                    <Link to="/about/assembly-instructions">Assembly Instructions</Link>
                     <Link to="/faqs">FAQs</Link>
 
                 </div>
                 <div className="footer-column">
                     <h4>About</h4>
-                    <Link to="/about-the-EVA-pod">The EVA Pod</Link>
-                    <Link to="/about-NASA-STELLA">NASA STELLA</Link>
-                    <Link to="/about-meet-CARMA">CARMA</Link>
+                    <Link to="/about/EVA-pod">The EVA Pod</Link>
+                    <Link to="/about/NASA-STELLA">NASA STELLA</Link>
+                    <Link to="/about/meet-CARMA">Meet CARMA</Link>
                     <Link to="/contact">Contact Us</Link>
                     <Link to="/privacy">Privacy</Link>
                 </div>
-                <div className="footer-column">
-                    <Link to="https://www.colorado.edu/center/spacegrant/" target="_blank">
-                        <img
-                            src={cosgc_logo}
-                            alt="COSGC Logo"
-                            className="footer-image"
-                        />
-                    </Link>
-                    <Link to="https://www.nasa.gov/" target="_blank">
-                        <img
-                            src="https://www.nasa.gov/wp-content/themes/nasa/assets/images/nasa-logo.svg"
-                            alt="NASA Logo"
-                            className="footer-image"
-                        />
-                    </Link>
+                <div className="footer-column footer-column-images">
+                    <div className="footer-images">
+                        <Link to="https://www.colorado.edu/center/spacegrant/" target="_blank">
+                            <img
+                                src={cosgc_logo}
+                                alt="COSGC Logo"
+                            />
+                        </Link>
+                        <Link to="https://www.nasa.gov/" target="_blank">
+                            <img
+                                src="https://www.nasa.gov/wp-content/themes/nasa/assets/images/nasa-logo.svg"
+                                alt="NASA Logo"
+                            />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </footer>
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,36 @@
-import { Link, NavLink } from "react-router-dom";
+import {Link, NavLink, useNavigate} from "react-router-dom";
+import {ApiError, authLogout, getAccessToken} from "../utils/api";
 import "../styles/Header.css";
+import {useEffect, useState} from "react";
 
 export default function Header() {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const [login, setLogin] = useState(false);
+
+    useEffect(() => {
+        const token = getAccessToken();
+        setLogin(!!token);
+    }, []);
+
+    const submitLogout = async (e: React.FormEvent) => {
+        e.preventDefault();
+        if (loading) return;
+        setLoading(true);
+        try {
+            await authLogout();
+            setLogin(false);
+            navigate("/");
+        } catch (err) {
+            if (err instanceof ApiError && err.status === 400) {
+                console.error("Logout failed", err);
+                alert("Logout failed. Please try again.");
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <header className="navbar">
             <div className="nav-container">
@@ -16,23 +45,27 @@ export default function Header() {
                             About
                         </NavLink>
                         <div className="dropdown-menu dropdown-menu-center">
-                            <Link to="/about/EVA-pod">The EVA Pod</Link>
-                            <Link to="/about/assembly-instructions">Assembly Instructions</Link>
-                            <Link to="/about/NASA-STELLA">NASA STELLA</Link>
-                            <Link to="/about/meet-CARMA">Meet CARMA</Link>
+                            <Link to="/about/EVA-pod" className="dropdown-item">The EVA Pod</Link>
+                            <Link to="/about/assembly-instructions" className="dropdown-item">Assembly Instructions</Link>
+                            <Link to="/about/NASA-STELLA" className="dropdown-item">NASA STELLA</Link>
+                            <Link to="/about/meet-CARMA" className="dropdown-item">Meet CARMA</Link>
                         </div>
                     </div>
                     <NavLink to="/faqs">FAQs</NavLink>
-                    <div className="dropdown">
-                        <NavLink to="/profile" className="dropdown-toggle">
-                            Profile
-                        </NavLink>
-                        <div className="dropdown-menu dropdown-menu-right">
-                            <Link to="/profile">View Profile</Link>
-                            <Link to="/settings">Settings</Link>
-                            <Link to="/">Logout</Link>
+                    {login && (
+                        <div className="dropdown">
+                            <NavLink to="/profile" className="dropdown-toggle">
+                                Profile
+                            </NavLink>
+                            <div className="dropdown-menu dropdown-menu-right">
+                                <Link to="/profile" className="dropdown-item">View Profile</Link>
+                                <Link to="/settings" className="dropdown-item">Settings</Link>
+                                <button onClick={submitLogout} className="dropdown-item" disabled={loading}>
+                                    {loading ? "Logging out..." : "Logout"}
+                                </button>
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </nav>
             </div>
         </header>
